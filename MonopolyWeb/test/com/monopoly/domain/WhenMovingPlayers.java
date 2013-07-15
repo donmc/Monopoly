d883fdee30e3d32783b13bc2ff3742d99944d5f1
Student <Student@TESTJOIN.improving.local>
2013-07-15 15:12:36 -0500

    init move tests

---
+++ MonopolyWeb/test/com/monopoly/domain/WhenMovingPlayers.java
@@ -0,0 +1,14 @@
+package com.monopoly.domain;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class WhenMovingPlayers {
+
+	@Test
+	public void shouldChangeLocation() {
+		fail("Not yet implemented");
+	}
+
+}
