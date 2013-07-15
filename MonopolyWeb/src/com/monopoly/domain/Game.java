e807ba2281a09931a235e00440cb686e03dcf3ca
Student <Student@STUDENTB2>
2013-07-15 12:06:36 -0500

    Tests and fix for first session.

--- MonopolyWeb/src/com/monopoly/domain/Game.java
+++ MonopolyWeb/src/com/monopoly/domain/Game.java
@@ -12,6 +12,9 @@ public class Game {
 		if(numberOfPlayers > TOKENS.length) {
 			throw new IllegalArgumentException("Too many players!");
 		}
+		if(numberOfPlayers < 2){
+			throw new IllegalArgumentException("Too few players!");
+		}
 		
 		for (int i = 0; i < numberOfPlayers; i++) {
 			Player player = new Player(TOKENS[i], board.getStartSquare());
