accea83f0b608f4e4f3bd49d4230183e28d0527d
Student <Student@TESTJOIN.improving.local>
2013-07-16 12:45:05 -0500

    add lux test

--- MonopolyWeb/test/com/monopoly/domain/WhenLandingOnSpecialtySquares.java
+++ MonopolyWeb/test/com/monopoly/domain/WhenLandingOnSpecialtySquares.java
@@ -7,8 +7,8 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 public class WhenLandingOnSpecialtySquares {
 
-@Test
-public void shouldCollect200WhenLandingOnGo () {
+	@Test
+	public void shouldCollect200WhenLandingOnGo () {
 
 		Board board = new Board();
 		Board.setDie1(new LoadedDie(2));
@@ -17,9 +17,24 @@ public void shouldCollect200WhenLandingOnGo () {
 		Player player = new Player("Hat", board.getSquares().get(37));
 		player.takeTurn(board);
 		
-		assertTrue ("Must not be on go square", player.getLocation().getName().equals("Go Square"));
+		assertTrue ("Must be on go square", player.getLocation().getName().equals("Go Square"));
 
 		assertEquals(1700, player.getMoney());
+	}
+
+@Test
+	public void shouldPay75WhenLandingOnLux () {
+	
+		Board board = new Board();
+		Board.setDie1(new LoadedDie(2));
+		Board.setDie2(new LoadedDie(1));
+		
+		Player player = new Player("Hat", board.getSquares().get(35));
+		player.takeTurn(board);
+		
+		assertTrue ("Must be on LUX square", player.getLocation().getName().equals("Luxury Tax"));
+	
+		assertEquals(1575, player.getMoney());
 	}
 }
 
